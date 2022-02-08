d01efa8d2302790355731a6a9bbf4f8b416a06dc
zoodogood <zoodogood@gmail.com>
2022-02-08 18:32:23 +0200

    *

---
+++ resources/modules/create/main.js
@@ -0,0 +1,38 @@
+const sections = {
+  "plainText": document.getElementById("plainText"),
+  "controll":  document.getElementById("textControll")
+};
+
+
+sections.plainText.querySelector("button").onclick = clickEvent => {
+  const plainContent = sections.plainText.querySelector("textarea").value;
+
+  if (!plainContent)
+    throw new Error("b");
+
+  const controllTextareaNode = sections.controll.querySelector("textarea");
+  controllTextareaNode.removeAttribute("disabled");
+  controllTextareaNode.value = placePointers( plainContent );
+
+  const buttonNode = document.createElement("button");
+  buttonNode.textContent = "Сохранить визуализацию в локальное хранилище";
+
+
+  sections.controll.append(buttonNode);
+};
+
+
+function placePointers( content ){
+  const POINTERS = {
+    "\\%":      /%/g,
+    
+    "%p(350)":  /\,/g,
+    "%p(800)":  /\./g,
+    "%p(1050)": /\.\.\./g
+  }
+
+  const replacer = ([point, reg]) => content = content.replaceAll(reg, (match) => `${ match }${point}`);
+
+  Object.entries( POINTERS ).forEach(replacer);
+  return content;
+}
