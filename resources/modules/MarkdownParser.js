c08a2ecdf9c00ae5892786c6d109b8647dbf06a2
zoodogood <zoodogood@gmail.com>
2022-02-09 08:58:49 +0200

    THIS A PERFECTABLE CLASS

--- resources/modules/MarkdownParser.js
+++ resources/modules/MarkdownParser.js
@@ -1,26 +1,53 @@
-let NO_ESCAPE = ``;
+
 
 class MarkdownParser {
   constructor(pointedContent){
-    this.inner   = document.createElement("section");
+    this.inner = document.createElement("section");
     this.inner.textContent = pointedContent;
     this.parseNode( this.inner );
   }
 
   parseNode(node){
+    if (node.classList.contains("no-parse"))
+      return node;
+
     const marks = this.constructor.MARKS;
     const container = [];
 
-    let reg = marks.map(({reg}) => `\\*?${ reg }`)
-      .join("|");
+    let content = node.textContent;
 
-    reg = new RegExp(reg, "g");
+    let reg = marks.map(({ reg, name }) => `(?<${ name }>${ reg })`).join("|");
+    reg = new RegExp(reg);
 
-    console.log(reg);
-    console.log([..."123**f**123".matchAll(reg)]);
+    while (true){
+      const match = content.match(reg);
+      if (match === null)
+        break;
 
+
+
+      if (match.index){
+        container.push(content.substring( 0, match.index ));
+        content = content.slice( match.index );
+      }
+      content = content.replace(match[0], "");
+
+      const innerContent = match.filter(Boolean).at(-1);
+      const {replacer} = this.#fetchMark(match.groups);
+
+      let parsed = replacer(innerContent);
+
+      if (parsed instanceof HTMLElement)
+        this.parseNode(parsed);
+
+      container.push( parsed );
+    }
+
+    container.push(content);
     node.innerHTML = "";
-    container.forEach(node.append);
+    
+    container.forEach(item => node.append(item));
+    return node;
   }
 
 
@@ -28,20 +55,36 @@ class MarkdownParser {
     return this.inner;
   }
 
+  #fetchMark(groups){
+    const groupName = Object.entries(groups)
+      .find(([name, value]) => value)
+      .at(0);
+
+    return this.constructor.MARKS.find(mark => mark.name === groupName);
+  }
+
 
   static MARKS = [
     {
+      name: "escaping",
+      reg: `\\\\`,
+      replacer: (content) => {
+        const node = document.createElement("span");
+        node.className = "escaping no-parse";
+        node.textContent = content;
+        return node;
+      }
+
+    },
+    {
       name: "bold",
-      reg: `\\*\\*(.+?)[^\\\\]\\*\\*`,
-      replacer: (full, group) => {
-        const boldNode = document.createElement("b");
-        boldNode.textContent = group;
-        return boldNode;
+      reg: `(?<!\\\\)\\*\\*(.+?)(?<!\\\\)\\*\\*`,
+      replacer: (content) => {
+        const node = document.createElement("b");
+        node.textContent = content;
+        return node;
       }
 
     }
   ]
 }
-
-
-delete NO_ESCAPE;
