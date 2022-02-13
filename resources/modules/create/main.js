2a4c91326a53b894d09a62e611d5bf3877e48bc9
Veleses-star <aziliptin44@gmail.com>
2022-02-13 09:23:38 +0700

    корректировка 4 теста

--- resources/modules/create/main.js
+++ resources/modules/create/main.js
@@ -61,10 +61,3 @@ function initLink(){
 
   sections.controll.append(node);
 }
-
-if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1   
-  var dpt = window.devicePixelRatio;
-  var widthM = window.screen.width * dpt;
-  var widthH = window.screen.height * dpt;
-  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
-}
--- resources/modules/index/main.js
+++ resources/modules/index/main.js
@@ -2,10 +2,3 @@ let hover = document.getElementsByClassName('hover-letter');
 for (let i=0; i<hover.length; i++) {
     hover[i].innerHTML = hover[i].innerHTML.replace(/(.)/g, '<span>$1</span>');
 }
-
-if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1       
-  var dpt = window.devicePixelRatio;
-  var widthM = window.screen.width * dpt;
-  var widthH = window.screen.height * dpt;
-  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
-}
