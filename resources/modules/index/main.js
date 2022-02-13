502268d97aef0e553df2ec3af384d6e4533260cf
Veleses-star <aziliptin44@gmail.com>
2022-02-13 09:11:09 +0700

    тест 3 (доп js)

--- resources/modules/index/main.js
+++ resources/modules/index/main.js
@@ -3,3 +3,9 @@ for (let i=0; i<hover.length; i++) {
     hover[i].innerHTML = hover[i].innerHTML.replace(/(.)/g, '<span>$1</span>');
 }
 
+if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1       
+  var dpt = window.devicePixelRatio;
+  var widthM = window.screen.width * dpt;
+  var widthH = window.screen.height * dpt;
+  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
+}
