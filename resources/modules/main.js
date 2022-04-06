953ba9bb4762690be36da69ceed2da2713cbcc48
Veleses-star <aziliptin44@gmail.com>
2022-04-06 23:51:08 +0700

    ultra bubbles

--- resources/modules/main.js
+++ resources/modules/main.js
@@ -3,7 +3,6 @@ function getListContent() {
 
   for(let i=1; i<=12; i++) {
     let li = document.createElement('li');
-    li.setAttribute('onclick', 'ChangeColor(this)')
     fragment.append(li);
   }
 
@@ -11,11 +10,3 @@ function getListContent() {
 }
 
 document.querySelector('ul').append(getListContent());
-
-
-function ChangeColor(Element) {
-    if (Element.style.background == 'rgba(255, 255, 255, 0.2)') {
-        Element.style.background = 'linear-gradient(90deg, #cfecd0, #a0cea7, #9ec0db)';
-    }
-    else Element.style.background = 'rgba(255, 255, 255, 0.2)';
-}
